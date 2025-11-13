3c74037d76c8e55b784fbbb90a5f7c63616ae21f
Vladislav Yegorov <77292451+sckmat@users.noreply.github.com>
2025-11-13 19:53:04 +0500

    Create UMS base

---
+++ src/main/java/com/university/usermanagementservice/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.university.usermanagementservice.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v0/users")
+public class UserController {
+
+    @GetMapping("/")
+    public Mono<ResponseEntity<Map<String, Object>>> getAllUsers(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("code",200);
+        map.put("message","success");
+        map.put("data",new ArrayList<>());
+        return Mono.just(ResponseEntity.ok().body(map));
+    }
+
+}
